31f36f983d161ddb12fba9e193a76efaf65aebbc
GabrielVieira21 <gabriel.vieira@1pay.com.br>
2025-09-17 13:53:49 -0300

    fix page bank

---
+++ app/components/header.tsx
@@ -0,0 +1,55 @@
+import {
+  Link,
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@radix-ui/react-navigation-menu";
+import shieldbank from "../assets/shieldbank.png";
+import Image from "next/image";
+import { Button } from "@/app/components/ui/button";
+
+export function Header() {
+  return (
+    <>
+      <header className="  opacity-80">
+        <div className="flex items-center justify-between max-w-7xl mx-auto px-4">
+          <Image src={shieldbank} alt="Shieldbank" width={160} height={60} />
+
+          <NavigationMenu>
+            <NavigationMenuList className="flex items-center gap-10 text-[18px]">
+              
+              <div className="flex gap-10">
+                <NavigationMenuLink href="/" className="hover:text-gray-400 text-white transition-colors">Quem somos</NavigationMenuLink>
+                <NavigationMenuLink href="/" className="hover:text-gray-400 text-white transition-colors">Shield Pay</NavigationMenuLink>
+                <NavigationMenuLink href="/blog" className="hover:text-gray-400 text-white transition-colors">Blog</NavigationMenuLink>
+                <NavigationMenuLink href="https://shieldbank.vagas.solides.com.br/" target="_blank" className="hover:text-gray-400 text-white transition-colors " >Carreiras</NavigationMenuLink>
+                <NavigationMenuLink href="/" className="hover:text-gray-400 text-white transition-colors">Contato</NavigationMenuLink>
+              </div>
+
+              
+              <div className="flex gap-4">
+                <Button
+                  asChild
+                  variant={"outline"}
+                  className="p-6 hover:bg-[#CCAA76] hover:border-0 hover:text-foreground"
+                >
+                  <a href="/acessar-conta">Acessar conta</a>
+                </Button>
+                <Button
+                  asChild
+                  variant={"outline"}
+                  className="p-6 hover:bg-[#CCAA76] hover:border-0 hover:text-foreground"
+                >
+                  <a href="https://wa.me/5511978802370/?text=Olá, Quero abrir minha conta Shield!" target="_blank">Abra sua conta</a>
+                </Button>
+              </div>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </header>
+    </>
+  );
+}
---
+++ app/home/index.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { Button } from "../components/ui/button";
+
+export function App() {
+  return (
+    <>
+      <div className=" w-full  h-[70%]  overflow-hidden flex  ">
+        <motion.div className="h-full flex items-center p-20 ">
+          <div className="w-full mt-10 flex flex-col  items-center">
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1 }}
+              className="text-center mb-10 mt-20"
+            >
+              Potencialize suas finanças: veja quanto sua empresa{" "}
+              <strong>economiza </strong>
+              mensalmente com a <strong>ShieldBank</strong>
+            </motion.p>
+            <motion.h1
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1 }}
+              className="text-8xl text-center font-light"
+            >
+              Tecnologia{" "}
+              <strong className="text-[#CCAA76]"> personalizada</strong> em
+              inteligência financeira
+            </motion.h1>
+            <motion.button className="text-center mt-10">
+              <Button
+                asChild
+                variant={"outline"}
+                className="p-6 hover:bg-[#CCAA76] hover:border-0 hover:text-foreground"
+              >
+                <a href="/login">Abra sua conta</a>
+              </Button>
+            </motion.button>
+          </div>
+        </motion.div>
+      </div>
+    </>
+  );
+}
