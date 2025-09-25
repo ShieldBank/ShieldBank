6a28b8ac33adbe5fafb224226fdeb45c00c34867
Gustavo Morais <gustavo.morais@1pay.com.br>
2025-09-25 12:47:13 -0300

    blogFull

--- app/components/news.tsx
+++ app/components/news.tsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-// const token = "d1c806b7b0d22cead5f5b432e8156e36"
-const token = "b014ea9f6b56d5b440f1db52dd5c48f5";
+const token = "d1c806b7b0d22cead5f5b432e8156e36"
+// const token = "b014ea9f6b56d5b440f1db52dd5c48f5";
 
 // const token = "f658eda930684da2b70b41255d121054"
 
 const urlBase = "https://gnews.io/api/v4/search";
 const lang = "pt";
 const matter = "publishedAt";
-const pageNews = Math.floor(Math.random() * 4) + 1;;
+const pageNews = Math.floor(Math.random() * 2) + 1;;
 
 // function to fetch news based on a query
 const news = async (query: string, page: number = pageNews, max: number = 100) => {
