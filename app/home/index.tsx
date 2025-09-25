e74251525bd6df6dae73de49752175dfa8ad153a
Gustavo Morais <gustavo.morais@1pay.com.br>
2025-09-25 16:48:48 -0300

    UpdateReponseHome

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -6,7 +6,7 @@ import { Button } from "../components/ui/button";
 export function App() {
   return (
     <>
-      <div className=" w-full  h-[75%] max-sm:h-[66%]  overflow-hidden flex  ">
+      <div className=" w-full overflow-hidden flex  ">
         <div
           // initial={{ opacity: 0 }}
           // animate={{ opacity: 1 }}
@@ -14,23 +14,23 @@ export function App() {
           //   duration: 0.05, // deixa mais rápido
           //   ease: "easeIn", // ou "linear", "easeOut", "easeInOut"
           // }}
-          className="h-full flex items-center p-20 max-sm:p-5"
+          className="h-full flex items-center max-sm:justify-center "
         >
           <div className="w-full mt-10 max-sm:mt-0 flex flex-col  items-center">
-            <p className="text-center mb-10 mt-25 max-sm:mt-35 max-sm:mb-2  max-sm:text-[0.8rem] ">
+            <p className="text-center mb-10 mt-25  max-sm:mt-35 max-sm:mb-2  max-sm:text-[1rem] ">
               Potencialize suas finanças: descubra quanto sua empresa pode
               <strong> economizar </strong>
               por mês com a <strong>ShieldBank</strong>
             </p>
-            <h1 className="text-8xl text-center font-light max-sm:text-6xl">
+            <h1 className="text-8xl text-center font-light max-sm:text-5xl">
               Tecnologia{" "}
               <strong className="text-[#CCAA76]"> personalizada</strong> em
               inteligência financeira
             </h1>
-            <span className="text-center mt-10  flex gap-12 max-sm:gap-8">
+            <span className="text-center mt-10  mb-5 flex gap-12 max-sm:gap-3">
               <Button
                 variant={"default"}
-                className="w-auto p-6 hover:bg-[#CCAA76] bg-transparent border border-amber-50 hover:border-0 hover:text-foreground text-2xl max-sm:text-[1rem] max-sm:w-30"
+                className="w-auto p-6 max-sm:p-4 hover:bg-[#CCAA76] bg-transparent border border-amber-50 hover:border-0 hover:text-foreground text-2xl max-sm:text-[0.9rem] max-sm:w-30"
               >
                 <a
                   href="https://wa.me/5511978802370/?text=Olá, Quero abrir minha conta Shield!"
@@ -41,7 +41,7 @@ export function App() {
               </Button>
               <Button
                 variant={"default"}
-                className="w-auto bg-[#CCAA76] border-0 p-6 hover:bg-foreground hover:border-0 hover:text-[#CCAA76] max-sm:w-50 text-2xl max-sm:text-[1rem] "
+                className="w-auto bg-[#CCAA76] border-0 p-6 max-sm:p-4 hover:bg-foreground hover:border-0 hover:text-[#CCAA76] max-sm:w-50 text-2xl max-sm:text-[0.9rem] "
               >
                 <a href="#produtos">Conheça Nossos Produtos</a>
               </Button>
